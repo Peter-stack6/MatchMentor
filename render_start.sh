06b8889990ef0f8db0b50c0e725b289657355b7d
Peter-stack6 <iseyemipiro@gmail.com>
2025-07-07 23:30:06 +0100

    Update render_start.sh

--- render_start.sh
+++ render_start.sh
@@ -1,13 +1,2 @@
-#!/usr/bin/env bash
-set -o errexit
-
-python manage.py makemigrations
-
-echo "📦 Running database migrations..."
-python manage.py migrate --noinput
-
-echo "👤 Creating superuser (if needed)..."
-python manage.py createsuperuser --noinput || true
-
 echo "🚀 Starting Gunicorn..."
 gunicorn backend.wsgi:application
